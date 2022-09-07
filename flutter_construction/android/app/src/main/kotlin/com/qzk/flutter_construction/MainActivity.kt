3d4e36edb4756ac8de57e9b35e972d116d0fd14c
qianzhikang <qzkqianzhikang@gmail.com>
2022-09-07 11:32:19 +0800

    state生命周期;stateless&stateful

---
+++ flutter_construction/android/app/src/main/kotlin/com/qzk/flutter_construction/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.qzk.flutter_construction
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ work/lib/placeholder-demo.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class PlaceholderDemo extends StatefulWidget {
+  const PlaceholderDemo({Key? key}) : super(key: key);
+
+  @override
+  State<PlaceholderDemo> createState() => PlaceholderState();
+}
+
+class PlaceholderState extends State<PlaceholderDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("图片占位符")),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            FadeInImage.assetNetwork(
+                placeholder: "assets/pictures/loading.gif",
+                image:
+                    "https://tuapi.eees.cc/dongman/pc/TUAPI-EEES-CC-2062114731.jpg"),
+            const Divider(),
+            CachedNetworkImage(
+              imageUrl: "https://tuapi.eees.cc/dongman/pc/TUAPI-EEES-CC-2062114731.jpg",
+              placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
